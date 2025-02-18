7a38be9f58e163c987f0c1cd7ad974a1cd1b49ac
Christerpher Hunter <djhunter67@gmail.com>
2025-02-18 08:05:30 -0500

    add: remove unused default 'add' method and test

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,3 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/tweetnacl_bindings.rs"));
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
